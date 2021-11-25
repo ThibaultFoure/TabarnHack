0708e996d0ac87ee1bf9029f4d0758a638bb0c14
Thibault <thibaultfoure@gmail.com>
2021-11-25 23:54:12 +0100

    request all playlists

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,9 @@
 
 namespace App\Controller;
 
+use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\RequireStrictTypesSniff;
+use Symfony\Component\HttpClient\HttpClient;
+
 class HomeController extends AbstractController
 {
     /**
@@ -22,5 +25,25 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->twig->render('Home/index.html.twig');
+    }
+
+    public function selectPlaylistsByCategory()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $newGet = array_map('trim', $_GET);
+
+            $client = HttpClient::create();
+            $response = $client->request('GET', 'https://api.deezer.com/search/playlist/?q=' . $newGet['title'] . '&index0&limit=15');
+
+            $statusCode = $response->getStatusCode();
+            $results = $response->toArray();
+            $playlists = [];
+            if ($statusCode === 200) {
+                foreach ($results as $playlists) {
+                    $playlists = $results['data'];
+                }
+                return $this->twig->render('Home/playlists.html.twig', ['playlists' => $playlists]);
+            }
+        }
     }
 }
